833fb69cc1bce6f07ffd2098f89e794f596ef442
okta-jenn <jennifer.galvin@okta.com>
2018-03-29 13:44:44 -0400

    Fixed syntax errors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var sourcePort = '1813';
 var destinationIP = 'localhost';
 var destinationPort = '1813';
 
-server.on("message", function (msg, rinfo) 
+server.on("message", function (msg, rinfo)
 {
   var code, username, password, packet, radiusDestination, radiusPort;
   packet = radius.decode({packet: msg, secret: secret});
@@ -16,7 +16,8 @@ server.on("message", function (msg, rinfo)
   // Pass everything else
   if (packet.code != 'Access-Request')
   {
-    var newPacket = radius.encode({
+    var newPacket = radius.encode(
+    {
       code: packet.code,
       secret: secret,
       attributes: packet.attributes
@@ -25,61 +26,67 @@ server.on("message", function (msg, rinfo)
   }
   else
   {
-    
+
     username = packet.attributes['User-Name'];
     password = packet.attributes['User-Password'];
-  
+
     var newPassword;
-  
+
     if ( ! password.trim().endsWith(",menu") || ! password.trim().endsWith(",push") )
-    { 
+    {
       newPassword = password + ",push";
       console.log("Access-Request for " + username + ", appending ,push for auto-push");
     }
     else
     {
       if (password.trim().endsWith(",menu"))
-      { 
+      {
         newPassword = password.trim().replace(",menu", "");
         console.log("Access-Request for " + username + ", request for menu, trimming ,menu from password field");
       }
-    
+
       if (password.trim.endsWith(",push"))
       {
         newPassword = password;
         console.log("Access-Request for " + username + ", request for push included, doing nothing");
       }
-  
-      var newPacket = radius.encode({
-      code: packet.code,
-      secret: secret,
-      attributes: [
-        ['NAS-IP-Address'], packet.attributes['NAS-IP-Address'],
-        ['User-Name'], packet.attributes['User-Name'],
-        ['User-Password'], newPassword]
-      });    
-  }
-     
-  switch(rinfo.address)
-  {   
-    case sourceIP:
-      radiusDestination = destinationIP;
-      radiusPort = destinationPort;
-      break;
-    case destinationIP:
-      radiusDestination = sourceIP;
-      radiusPort = sourcePort;      
-  }
-  
-  console.log("Sending newPacket");
-  server.send(newPacket, 0, response.length, radiusDestinationPort, radiusDestinationIP, function(err, bytes) {
-    if (err) {
-      console.log("Error sending response to ", destinationIP);
+
+      var newPacket = radius.encode(
+      {
+        code: packet.code,
+        secret: secret,
+        attributes: [
+          ['NAS-IP-Address'], packet.attributes['NAS-IP-Address'],
+          ['User-Name'], packet.attributes['User-Name'],
+          ['User-Password'], newPassword]
+      });
     }
-  });
+
+    switch(rinfo.address)
+    {
+      case sourceIP:
+        radiusDestination = destinationIP;
+        radiusPort = destinationPort;
+        break;
+      case destinationIP:
+        radiusDestination = sourceIP;
+        radiusPort = sourcePort;
+        break;
+    }
+
+    console.log("Sending newPacket");
+    server.send(newPacket, 0, response.length, radiusDestinationPort, radiusDestinationIP, function(err, bytes)
+    {
+      if (err)
+        { console.log("Error sending response to ", destinationIP); }
+    });
+
+  }  // end of else
+
 });
 
-server.on("listening", function () {
+server.on("listening", function ()
+{
   var address = server.address();
   console.log("radius server listening " +
       address.address + ":" + address.port);
